3ee71b796ee49e73c72523bc3e352e4a0ebae979
SolarDefender <vladimirkalinin67@gmail.com>
2024-01-17 16:53:03 +0100

    Fixed scaling of containers

--- frontend/src/Pages/StorePage.jsx
+++ frontend/src/Pages/StorePage.jsx
@@ -73,7 +73,7 @@ function StorePage() {
                         <p>{item.Name}</p>
                         <p>{item.Description}</p>
                         <div className='price'><p>${item.Price.toFixed(2)}</p></div>
-                        <button className='store-button' onClick={() => cartHandler(item)}>Buy</button>
+                        <button className='store-button' onClick={() => cartHandler(item)}>Add to cart</button>
                         <p>Amount: {item.Amount}</p>
                     </div>
                 </div>
